d5debbb852b7a529d1391e66407647a80a9c78ac
Fran-Driussi <frandriussi@gmail.com>
2023-01-10 13:57:24 -0300

    generando carrito

--- js/script.js
+++ js/script.js
@@ -3,29 +3,86 @@ let emailUsuario = document.getElementById('emailAddress');
 let password = document.getElementById('password');
 let btnLogin = document.getElementById('btnLogin');
 let btnVaciar = document.getElementById('btnVaciarLocalStorage');
-let buy = document.getElementById('btnBuy');
+let card = document.querySelectorAll('.card');
 
 
-btnLogin.addEventListener('click', () => {
+btnLogin.addEventListener('click', (e) => {
     localStorage.setItem('email', JSON.stringify(emailUsuario));
     localStorage.setItem('password', JSON.stringify(password));
-    alert('Bienvenido: ' + emailUsuario.value);
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
+    Toast.fire({
+        icon: 'success',
+        title: 'Signed in successfully'
+    })
+
+    e.preventDefault();
+
+
 
 })
 
-btnVaciar.addEventListener('click', () => {
+btnVaciar.addEventListener('click', (e) => {
     localStorage.clear();
-    alert('Se han borrado todos los datos');
+    Swal.fire({
+        title:  'Se han borrado todos los datos',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+    })
+    e.preventDefault();
 })
 
 
-buy.onclick = () => {
-    localStorage.setItem('compra', buy.value);
-    alert('Se agrego el producto al carrito');
+card.forEach((card) =>{
+    card.addEventListener('click',(e)=>{
+        leerDatos(e.target.parentElement)
+    })
+})
+
+let carrito = [];
+
+function leerDatos(producto){
+    const infoProd= {
+        titulo : producto.querySelector('.card-title').textContent,
+        precio: producto.querySelector('.precio').textContent,
+        texto: producto.querySelector('.card-text').textContent,
+        id: querySelectorAll(".btn").getAttribute,
+    }
+    console.log(infoProd)
+
+
+carrito= [...carrito, infoProd]
+carroHTML();
 }
 
+const carro = document.querySelector('#carrito')
+
+function carroHTML(){
+    carrito.forEach((producto) =>{
+        const row= document.createElement('p')
+        row.innerHTML = `
+        <div class ="container">
+        <h5>${producto.titulo}</h5>
+        <p>${producto.precio}</p>
+        <p>${producto.texto}</p>
 
 
+        <button class="btn btn-danger" id="${producto.id}"> Eliminar </button>
+        </div>
+        `;
+        carro.appendChild(row);
+    })
+}
 
 
 
